25dee242ad2b083f71a24faa8a9830bafa91916b
Kinshuk Ghildial <dev.kghildial@gmail.com>
2018-01-03 21:08:23 +0530

    Define input component state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 
 //ES6 class (like a JS object)
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
     return <input onChange={ (event) => console.log(event.target.value) } />;
   }
 
 }
-//statement for exporting code
+//statement for exporting code 
 export default SearchBar;
