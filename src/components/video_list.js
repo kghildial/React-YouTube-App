3953362e6ca5ed76c78759cd4ea04700ea84f294
Kinshuk Ghildial <dev.kghildial@gmail.com>
2018-01-04 12:56:39 +0530

    Import VideoListItem comp.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,11 +2,12 @@
 // Thus it can be a functional component
 
 import React from 'react';
+import VideoListItem from './video_list_item'
 
 const VideoList =  (props) => {
   return (
     <ul className="col-md-4 list-group">
-      { props.videos.length }
+      
     </ul>
   )
 }
