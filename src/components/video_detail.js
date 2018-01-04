a7814f087b367a3b3f76e8ce8495b96a43c96624
Kinshuk Ghildial <dev.kghildial@gmail.com>
2018-01-04 14:22:27 +0530

    Create iframe url

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId;
+  const url = 'https://www.youtube.com/embed/' + videoId;
+  
   return (
     <div className="video-detail">
       <div className="embed-responsive embed-responsive-16by9">
