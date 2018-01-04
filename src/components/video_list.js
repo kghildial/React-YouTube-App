4f50bf025fae58933fbf85b25091de049d33b4c7
Kinshuk Ghildial <dev.kghildial@gmail.com>
2018-01-04 12:28:11 +0530

    Create VideoList comp.

---
+++ src/components/video_list.js
@@ -0,0 +1,13 @@
+// Since this just takes and displays data (ie. a passive component)
+// Thus it can be a functional component
+
+import React from 'react';
+
+const VideoList =  () => {
+  return (
+    <ul className="col-md-4 list-group">
+    </ul>
+  )
+}
+
+export default VideoList;
