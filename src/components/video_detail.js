bb7a61415d188c87bb82e007c3a4e5dbc4ed7609
Kinshuk Ghildial <dev.kghildial@gmail.com>
2018-01-04 14:26:43 +0530

    Update coment

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoDetail = ({video}) => {
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
-  // String interpolation: exactly the same as 'https://www.youtube.com/embed/' + videoId; 
+  // ES6:: String interpolation: exactly the same as 'https://www.youtube.com/embed/' + videoId; 
 
   return (
     <div className="video-detail">
