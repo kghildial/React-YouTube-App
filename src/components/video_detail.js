30b824769fb33b3d67f9a165a0082dfa39ce64a1
Kinshuk Ghildial <dev.kghildial@gmail.com>
2018-01-04 14:20:38 +0530

    Populate video title & desc

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const VideoDetail = (props) => {
+const VideoDetail = ({video}) => {
   return (
     <div className="video-detail">
       <div className="embed-responsive embed-responsive-16by9">
       <iframe className="embed-responsive-item"></iframe>
       </div>
       <div className="details">
-        <div></div>
-        <div></div>
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
       </div>
     </div>
   );
